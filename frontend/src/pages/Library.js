2e4db17ad226bc555bb0fb0b68c6e748fdbc3190
Céline Busson <cecoue@hotmail.com>
2024-11-10 15:03:24 +0100

    librairie minimum première mise en ligne

--- frontend/src/pages/Library.js
+++ frontend/src/pages/Library.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from '../components/Header'
 import Footer from '../components/Footer';
 import Book from './../components/Book'
@@ -11,27 +11,119 @@ const Library = () => {
 
     const { isAuthenticate }= useContext(authContext)
     const { bookListe, getBookListe }=useContext(libraryContext);
- 
 
+    const [selectedCat, setSelectedCat]=useState('');
+    const [selectedTheme, setSelectedTheme]=useState('');
 
+    const categories = ['Enfant', 'Adulte'];
     
+    const getUniqueElements=(array)=> {
+        return array.filter((value, index, self) => {
+            return self.indexOf(value) === index;
+        });
+    }
+
+    const getTheme=(cat)=>{
+        const newListe = bookListe.filter((el)=>el.cat.toLowerCase() ===cat)
+        const themeListe = []
+        for(let livre of newListe){
+            for(let theme of livre.theme){
+                themeListe.push(theme)
+            }
+        }  
+        const liste = getUniqueElements(themeListe)  
+        return(liste)    
+    }
+
+    const getThemeListe = ()=>{
+        let arrayCat=[];
+        let obj;
+        for(let cat of categories){
+            const liste =getTheme(cat.toLocaleLowerCase())
+            obj = {
+                nom:cat,
+                liste:liste
+            }
+            arrayCat.push(obj)
+        }
+        return(arrayCat)
+    }
+
+    const selectCat=(cat)=>{
+        setSelectedCat(cat.nom)
+        setSelectedTheme("")
+    }
+    
+    const selectTheme=(cat, theme)=>{
+        setSelectedCat(cat.nom)
+        setSelectedTheme(theme)
+    }
+
+
     return (
         <div>
           <Header />
           <main className='libraryPage'>
-            <div className='libraryNav'></div>
+            <div className='libraryNav'>
+                {
+                    getThemeListe().map((cat, index)=>(
+                        <div key={index} className='libraryNav_liste'>
+                            <h3 onClick={()=>{selectCat(cat)}}>{cat.nom}</h3>
+                            {cat.liste.map((theme, index)=>(
+                                <p onClick={()=>{selectTheme(cat, theme)}} key={index}>{theme}</p>
+                            ))}
+                        </div>
+                    ))
+                }
+            </div>
             <div className='library'>
+                <div className='categorie'>
+                {selectedCat.length>=1
+                    ?
+                    selectedTheme.length>=1
+                        ?
+                        <div>
+                            <h1>Livres {selectedCat}</h1>
+                            <h2>{selectedTheme}</h2>
+                        </div>
+                        :<h1>Livres {selectedCat}</h1>
+                    :<h1>Tous les livres</h1>
+                }
+                </div>
+                <div className='allBooks'>
                 {
                     bookListe&&
                     isAuthenticate&&
-                    bookListe.map((book, index)=>
+                    selectedCat.length>=1
+                        ?selectedTheme.length>=1 
+                        ?bookListe
+                        .filter((book)=>book.cat===selectedCat.toLowerCase() )
+                        .filter((book)=>book.theme.includes(selectedTheme) )
+                        .map((book, index)=>
                     ( <Book 
                         book={book} 
-                        key={index} 
+                        key={index}
                         getBookListe={getBookListe}
                         />)
-                    )
-                }
+                    ):bookListe
+                    .filter((book)=>book.cat===selectedCat.toLowerCase() )
+                    //.filter((book)=>book.theme.includes(selectedTheme) )
+                    .map((book, index)=>
+                ( <Book 
+                    book={book} 
+                    key={index}
+                    getBookListe={getBookListe}
+                    />)
+                )
+                    :bookListe
+                    .map((book, index)=>
+                ( <Book 
+                    book={book} 
+                    key={index}
+                    getBookListe={getBookListe}
+                    />)
+                )
+                }</div>
             </div>
         </main>
           <Footer/>
